5760539d0fc4196743c9c7b61d5817760085e5fd
Sanjeet Kanwar <chaneetsk@hotmail.com>
2025-04-14 23:39:42 +1000

    updated sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -10,17 +10,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'yearly',
       priority: 1,
     },
-    // {
-    //   url: `${baseUrl}/bs-to-ad`,
-    //   lastModified: new Date(),
-    //   changeFrequency: 'yearly',
-    //   priority: 0.8,
-    // },
-    // {
-    //   url: `${baseUrl}/ad-to-bs`,
-    //   lastModified: new Date(),
-    //   changeFrequency: 'yearly',
-    //   priority: 0.8,
-    // },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.7,
+    },
   ];
 }
